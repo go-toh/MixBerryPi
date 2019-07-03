ea1d760d24c7cb89f5c4a02a97907de5d26b9138
kurumi <kurumiallergy@gmail.com>
2019-07-03 22:22:47 +0900

    Directory structure change

---
+++ src/main.py
@@ -0,0 +1,17 @@
+""" main program """
+import cv2
+import numpy
+import time
+
+print("hello world")
+"""class Kensyutu:
+
+    def __init__():
+        
+def main():
+    hoge = Kensyutu()
+
+if __name__ == "__main__":
+    main()
+
+"""
