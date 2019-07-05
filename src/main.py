d52d569c2e50a8a886adda4af005f1d91363fc1c
kurumi <kurumiallergy@gmail.com>
2019-07-05 15:50:19 +0900

    add function

--- src/main.py
+++ src/main.py
@@ -3,15 +3,18 @@ import cv2
 import numpy
 import time
 
-print("hello world")
-"""class Kensyutu:
+def video_cap():
+    capture = cv2.VideoCapture(0)
+    time.sleep(1)
+    ret, image = capture.read()
+    return ret,image
 
-    def __init__():
-        
-def main():
-    hoge = Kensyutu()
+ret, image = video_cap()
 
-if __name__ == "__main__":
-    main()
-
-"""
+if ret == True:
+    cv2.imshow("image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+else:
+    print("ERROR!!")
