d096414117bfa46287154cd0bf867af2d15c2a5a
kurumi <kurumiallergy@gmail.com>
2019-07-16 21:31:57 +0900

    delete print

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import time
 class ImageData:
 
     def _image_cap(self):
-        print("imagecap")
         self.cap = cv2.VideoCapture(0)
         time.sleep(1)
         _, frame = self.cap.read()
@@ -15,7 +14,6 @@ class ImageData:
 class Display:
 
     def _image_show(self, image):
-        print(image)
         self.image = image
         cv2.imshow("image", self.image)
         cv2.waitKey(0)
