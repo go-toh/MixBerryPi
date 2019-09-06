fd520aa33a297841f4ee5362b9396c6188bdc67b
kurumi <kurumiallergy@gmail.com>
2019-09-06 14:30:54 +0900

    add coment

--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
 """ main program """
-import cv2
 import time
+import cv2
 
+#画像データ管理
 class ImageData:
 
+    #画像取得
     def _image_cap(self):
         self.cap = cv2.VideoCapture(0)
         time.sleep(1)
         _, frame = self.cap.read()
-        
         return frame
 
+#表示管理
 class Display:
 
+    #画像表示
     def _image_show(self, image):
         self.image = image
         cv2.imshow("image", self.image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+#main関数
 def main():
     imagecap = ImageData()
     image = imagecap._image_cap()
