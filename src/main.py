6e4f863a60764794c5ece30097e9897f9852218a
kurumi <kurumiallergy@gmail.com>
2019-10-10 11:27:47 +0900

    add

--- src/main.py
+++ src/main.py
@@ -1,33 +1,30 @@
 """ main program """
+import picamera
+import io
+import os
+import datetime
 import time
 import cv2
 
 #画像データ管理
-class ImageData:
 
-    #画像取得
-    def _image_cap(self):
-        self.cap = cv2.VideoCapture(0)
-        time.sleep(1)
-        _, frame = self.cap.read()
-        return frame
+#画像取得
+def image_cap(time):
+    with picamera.Picamera as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        time.sleep(3)
+        camera.capture(get_time()+".jpg")
 
-#表示管理
-class Display:
-
-    #画像表示
-    def _image_show(self, image):
-        self.image = image
-        cv2.imshow("image", self.image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+#時刻取得
+def get_time():
+    dt_time = datetime.datetime.now()
+    nowtime = dt_time.strftime("%Y%m%d%H%M%S")
+    return nowtime
 
 #main関数
 def main():
-    imagecap = ImageData()
-    image = imagecap._image_cap()
-    display = Display()
-    display._image_show(image)
-
+    image_cap()
+    
 if __name__ == "__main__":
     main()
