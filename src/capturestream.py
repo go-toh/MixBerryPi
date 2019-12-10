a9e41e93087b81c8f967b927ff0a27547c776d1a
KurumiAllergy <kurumiallergy@gmail.com>
2019-12-11 00:16:36 +0900

    add

--- src/capturestream.py
+++ src/capturestream.py
@@ -2,9 +2,25 @@ import picamera
 import numpy as np
 import cv2
 import io
+import datetime
+import time
 
+#画像取得
+def image_cap():
+    camera.resolution = (3280, 1845)#16:9
+    camera.capture(get_time()+".jpg")
+    camera.resolution = (480, 270)
+    time.sleep(0.1)
+
+#時刻取得
+def get_time():
+    dt_time = datetime.datetime.now()
+    nowtime = dt_time.strftime("%Y%m%d%H%M%S")
+    return nowtime
+
+#ストリーム取得
 with picamera.PiCamera() as camera:
-    camera.resolution = (320,240)
+    camera.resolution = (480, 270)
     camera.framerate = 30
     stream = io.BytesIO()
     key=0
@@ -17,8 +33,12 @@ with picamera.PiCamera() as camera:
         stream.truncate()
         stream.seek(0)
         
-        #Esc key
         key = cv2.waitKey(100)
+        print(key)
+        #Enter Key
+        if key == 13:
+            image_cap()
+        #Esc key
         if key == 27:
             break
 
---
+++ src/object_detection.py
@@ -0,0 +1,8 @@
+from tflite_runtime.interpreter import Interpreter
+import picamera
+import numpy
+import cv2
+import io
+import time
+
+
